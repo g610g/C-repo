5ee85de9bcbbbc79ee7b06fb11e0e929780178a6
g610g <gio.gonzales@carsu.edu.ph>
2023-09-13 09:26:43 -0400

    fi nal

--- GonzalesAct1.c
+++ GonzalesAct1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX_REGISTER 32
+#define MAX_REGISTER 31
 #define REPRESENTATION 5
 int parseInput(char* input)
 {
@@ -9,14 +9,37 @@ int parseInput(char* input)
 }
 void registerToBinary()
 {
+  char input[3], binaryRepresentation[] = {0, 0, 0, 0, 0};
+  int registerNumber, i = REPRESENTATION - 1, printValue;
+  printf("Enter value: ");
+  scanf("%s", input);
+  if (!(input[0] == 'r' || input[0] == 'R'))
+  {
+    printf("invalid register\n");
+    return;
+  }
+  registerNumber = parseInput(input);
+  printValue = registerNumber;
+  if (registerNumber > MAX_REGISTER || registerNumber < 0)
+  {
+    printf("Invalid register\n");
     
+    return;
+  }
+  while(registerNumber > 0)
+  {
+    binaryRepresentation[i] = registerNumber % 2;
+    registerNumber /= 2;
+    i--;
+  }
+    printf("You have entered register number %d with machine code equivalent of ", printValue);
+  for (i = 0; i < REPRESENTATION; i++)
+  {
+    printf("%d", binaryRepresentation[i]);
+  }
+    printf(", a five bits representation.\n");
 }
 int main(void)
 { 
-  char input[3];
-  int registerNumber;
-  printf("Enter value: ");
-  scanf("%s", input);
-  printf("%s\n",input);
-  printf("%d\n", parseInput(input));
+  registerToBinary();
 }
